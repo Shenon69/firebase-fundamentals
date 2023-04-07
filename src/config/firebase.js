c905affd96b8d613cedb5cdd2b3c5040ce1dcb56
Shenon69 <ask.tikki69@gmail.com>
2023-04-07 11:47:36 +0530

    feat: firebase authentication with email,password and google auth

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsbPbbHs4wuCXRpio2tBLwmcLCpHn3usY",
+  authDomain: "fir-basics-4a385.firebaseapp.com",
+  projectId: "fir-basics-4a385",
+  storageBucket: "fir-basics-4a385.appspot.com",
+  messagingSenderId: "850758618279",
+  appId: "1:850758618279:web:0a1adfde356684a61e5f82",
+  measurementId: "G-MH2E93B1NG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
